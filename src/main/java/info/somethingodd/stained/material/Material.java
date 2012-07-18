5cc15f7b575be8c77cf6bf14da20f5b2f13c1f1c
Gordon Pettey <petteyg359@gmail.com>
2012-07-18 14:21:17 -0500

    GPL

--- src/main/java/info/somethingodd/stained/material/Material.java
+++ src/main/java/info/somethingodd/stained/material/Material.java
@@ -11,16 +11,17 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package info.somethingodd.stained;
+package info.somethingodd.stained.material;
 
-import info.somethingodd.stained.block.*;
+import info.somethingodd.stained.Color;
+import info.somethingodd.stained.TextureCoordinates;
 import org.bukkit.plugin.Plugin;
+import org.getspout.spoutapi.block.design.GenericCuboidBlockDesign;
 import org.getspout.spoutapi.block.design.Texture;
 import org.getspout.spoutapi.material.Block;
 import org.getspout.spoutapi.material.CustomBlock;
-import org.getspout.spoutapi.material.MaterialData;
+import org.getspout.spoutapi.material.block.GenericCuboidCustomBlock;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,13 +30,12 @@ import java.util.Map;
  * @author Gordon Pettey (petteyg359@gmail.com)
  */
 public class Material {
-    private Plugin plugin;
-    private String name;
-    private Block source;
-    private TextureCoordinates.TextureCoordinate textureCoordinate;
-    private BlockType blockType;
-    private final Map<Color, CustomBlock> blocks = new HashMap<Color, CustomBlock>();
-    private static Texture texture;
+    protected Plugin plugin;
+    protected String name;
+    protected Block source;
+    protected TextureCoordinates.TextureCoordinate textureCoordinate;
+    protected final Map<Color, CustomBlock> blocks = new HashMap<Color, CustomBlock>();
+    protected static Texture texture;
 
     /**
      * @param plugin Plugin owning block
@@ -43,29 +43,14 @@ public class Material {
      * @param source Base block for non-spout clients
      */
     public Material(Plugin plugin, String name, Block source) {
-        this(plugin, name, source, BlockType.CUBE);
-    }
-
-    /**
-     * @param plugin Plugin owning block
-     * @param name Base name of new block
-     * @param source Base block for non-spout clients
-     * @param blockType Type of block
-     */
-    public Material(Plugin plugin, String name, Block source, BlockType blockType) {
         this.plugin = plugin;
         this.name = name;
         this.source = source;
-        this.blockType = blockType;
         textureCoordinate = TextureCoordinates.getTextureCoordinate(source);
     }
 
     public static void setTexture(Texture texture) {
         Material.texture = texture;
-    }
-
-    public static Texture getTexture() {
-        return texture;
     }
 
 
@@ -85,35 +70,22 @@ public class Material {
         return blocks.get(color);
     }
 
+    /**
+     * Instantiates a colored block of this material and returns it
+     * @param color Color for block
+     * @return colored block
+     */
     public CustomBlock makeBlock(Color color) {
         CustomBlock block;
         String name = color.getName() + " " + getName();
-        plugin.getLogger().info("Adding " + name);
-        int[] textureId = textureIdForColor(color, textureCoordinate.getTextureId());
-        plugin.getLogger().info("tid: " + Arrays.toString(textureId));
-        if (BlockType.GLASS.equals(blockType)) {
-            block = new GlassBlock(plugin, name, texture, textureId, source);
-        } else if (BlockType.GLOWSTONE.equals(blockType)) {
-            block = new GlowstoneBlock(plugin, name, texture, textureId, source, color);
-        } else if (BlockType.OBSIDIAN.equals(blockType)) {
-            block = new ObsidianBlock(plugin, name, texture, textureId, source, color);
-        } else if (BlockType.SLAB.equals(blockType)) {
-            int[] textureIdLow = textureIdForColor(color, textureCoordinate.getTextureIdLow());
-            int[] textureIdHigh = textureIdForColor(color, textureCoordinate.getTextureIdHigh());
-            plugin.getLogger().info("tidL: " + Arrays.toString(textureIdLow));
-            plugin.getLogger().info("tidH: " + Arrays.toString(textureIdHigh));
-            block = new SlabBlock(plugin, name, texture, textureIdLow, textureId, textureIdHigh, source);
-        } else if (BlockType.STONE.equals(blockType)) {
-            block = new StoneBlock(plugin, name, texture, textureId, source, color);
-        } else {
-            block = new CubeBlock(plugin, name, texture, textureId, source);
-        }
+        block = new GenericCuboidCustomBlock(plugin, name, new GenericCuboidBlockDesign(plugin, texture, textureIdForColor(color, textureCoordinate.getTextureId()), 0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F));
         blocks.put(color, block);
         return block;
     }
 
     public int[] textureIdForColor(Color color, int[] textureId) {
-        int rowSize = getTexture().getWidth() / 16;
+        int rowSize = texture.getWidth() / texture.getSpriteSize();
+        plugin.getLogger().info("Texture size: " + texture.getWidth() + "x" + texture.getHeight() + "-" + texture.getSpriteSize());
         int[] coloredTextureId = new int[textureId.length];
         for (int i = 0; i < textureId.length; i++) {
             coloredTextureId[i] = textureId[i] + (rowSize * color.getRow());
@@ -123,12 +95,11 @@ public class Material {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder("Material{");
+        StringBuilder sb = new StringBuilder(getClass().getSimpleName()).append("{");
         sb.append("plugin: ").append(plugin.toString()).append(", ");
         sb.append("name: \"").append(name).append("\", ");
         sb.append("source: ").append(source.toString()).append(", ");
-        sb.append("textureCoordinate: ").append(textureCoordinate.toString()).append(", ");
-        sb.append("blockType: ").append(blockType.name()).append("}");
+        sb.append("textureCoordinate: ").append(textureCoordinate.toString()).append("}");
         return sb.toString();
     }
 
@@ -142,14 +113,5 @@ public class Material {
         if (!(obj instanceof Material)) return false;
         if (!getName().equals(((Material) obj).getName())) return false;
         return (getSource().equals(((Material) obj).getSource()));
-    }
-
-    public enum BlockType {
-        CUBE,
-        GLASS,
-        GLOWSTONE,
-        OBSIDIAN,
-        STONE,
-        SLAB
     }
 }
